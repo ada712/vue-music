9793f6d70e96135f9114a0978d00628ca6d3f396
2014874201 <1152579039@qq.com>
2018-01-29 14:39:21 +0800

    2018-1-29 14:39:07 添加注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,9 +1,19 @@
 'use strict'
+// node自带的文件路径工具
 const path = require('path')
+// 工具函数集合
 const utils = require('./utils')
+ // 配置文件
 const config = require('../config')
+ // 工具函数集合
 const vueLoaderConfig = require('./vue-loader.conf')
 
+/**
+ * 获得绝对路径
+ * @method resolve
+ * @param  {String} dir 相对于本文件的路径
+ * @return {String}     绝对路径
+ */
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -16,15 +26,21 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
+     // 编译输出的静态资源根路径
     path: config.build.assetsRoot,
+    // 编译输出的文件名
     filename: '[name].js',
+    // 正式发布环境下编译输出的上线路径的根路径
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
   resolve: {
+    // 自动补全的扩展名
     extensions: ['.js', '.vue', '.json'],
+    // 路径别名
     alias: {
+       // 例如 import Vue from 'vue'，会自动到 'vue/dist/vue.common.js'中寻找
       'vue$': 'vue/dist/vue.esm.js',
       'common': resolve('src/common'),
       'components': resolve('src/components'),
@@ -35,16 +51,22 @@ module.exports = {
   module: {
     rules: [
       {
+        // 处理 vue文件
+        // https://github.com/vuejs/vue-loader
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
       {
+        // 编译 js
+        // https://github.com/babel/babel-loader
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
+        // 处理图片文件
+        // https://github.com/webpack-contrib/url-loader
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
@@ -53,6 +75,7 @@ module.exports = {
         }
       },
       {
+        // 处理字体文件
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@
 const path = require('path')
 
 module.exports = {
+  // dev 开发环境
   dev: {
 
     // Paths
@@ -15,7 +16,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
