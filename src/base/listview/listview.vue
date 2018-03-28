cba0175f9dfec5749706fbb3282303f5f0b43288
2014874201 <1152579039@qq.com>
2018-03-28 15:44:30 +0800

    右端的字母列表

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,32 +1,185 @@
 <template>
-<scroll class="Listview " :data="data">
-  <ul>
-    <li v-for="group in data" class="list-group" :key="group.key" ref="listGroup">
-      <h2 class="list-group-title"> {{group.title}} </h2>
+  <scroll class="Listview" 
+    :data="data" 
+    ref="listView"
+    :listenScroll="listenScroll"
+    :probeType="probeType"
+    @scroll="scroll"
+  >
+    <ul>
+      <li v-for="group in data" class="list-group" :key="group.key" ref="listGroup">
+        <h2 class="list-group-title"> {{group.title}} </h2>
+        <ul>
+          <li @click="selectItem(item)" class="list-group-item" v-for="item in group.items" :key="item.key">
+            <img v-lazy="item.avatar" alt="" class="avatar">
+            <span class="name"> {{item.name}} </span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    
+    <!--右侧快速定位列表-->
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
       <ul>
-        <li class="list-group-item" v-for="item in group.items" :key="item.key">
-          <img v-lazy="item.avatar" alt="" class="avatar">
-          <span class="name"> {{item.name}} </span>
+        <li class="item" v-for="(item, index) in shortcutList" 
+          :data-index="index" 
+          :key="item.key"
+          :class="{'current' : currentIndex === index}"
+        >
+          {{item}}
         </li>
       </ul>
-    </li>
-  </ul>
-</scroll>
+    </div>
+    <div class="loading-container" v-show="!data.length">
+      <loading></loading>
+    </div>
+    <!-- <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title"> {{fixedTitle}} </h1>
+    </div> -->
+  </scroll>
 </template>
 <script>
-import Scroll from 'base/scroll/scroll.vue'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import { getData } from 'common/js/dom'
 
-export default {
-  props: {
-    data: {
-      type: Array,
-      default: []
+  const ANCHOR_HEIGHT = 18
+  const TITLE_HEIGHT = 30
+  export default {
+    created() {
+      this.touch = {}
+      this.listenScroll = true
+      this.listHeight = []
+      this.probeType = 3
+    },
+    data() {
+      return {
+        scrollY: -1,
+        currentIndex: 0,
+        diff: -1
+      }
+    },
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => { // map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值。
+          return group.title.substr(0, 1)
+        })
+      }
+      // fixedTitle() {
+      //   if (this.scrollY > 0) return
+      //   return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+      // }
+    },
+    components: {
+      Scroll,
+      Loading
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this.calcHeight()
+        }, 20)
+      },
+
+      // 监听scrollY 获取currentIndex
+      scrollY(newY) {
+        const listHeight = this.listHeight
+        // top
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        // middle
+        for (let i = 0; i < listHeight.length - 1; i++) {
+          let height1 = listHeight[i]
+          let height2 = listHeight[i + 1]
+          // 向上滚动srcollY的值为负 所以加上负号
+          if (-newY >= height1 && -newY < height2) {
+            this.currentIndex = i
+            this.diff = height2 + newY
+            // console.log(this.diff)
+            // console.log(this.currentIndex)
+            return
+          }
+        }
+        // bottom
+
+        this.currentIndex = listHeight.length - 2
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        if (this.fixedTop === fixedTop) {
+          return
+        }
+        this.fixedTop = fixedTop
+        // console.log(this.fixedTop)
+        this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
+      }
+    },
+    methods: {
+      refresh() {
+        this.$refs.listView.refresh()
+      },
+      onShortcutTouchStart(e) {
+        // 获取当前触摸的index
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0]
+        this.touch.y1 = firstTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0]
+        this.touch.y2 = firstTouch.pageY
+
+        // 向下取整
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+        // 获取移动的距离
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+        this._scrollTo(anchorIndex)
+      },
+      // 监听scroll组件派发的scroll事件 获取滚动时的pos值
+      scroll(pos) {
+        this.scrollY = pos.y
+        // console.log(this.scrollY)
+      },
+      _scrollTo(index) {
+        if (!index && index !== 0) {
+          return
+        }
+
+        // 判断index上下限
+        if (index < 0) {
+          index = 0
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2
+        }
+        this.scrollY = -this.listHeight[index]
+        this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 0)
+      },
+      // 计算每一个singer list的height
+      calcHeight() {
+        this.listHeight = []
+        const list = this.$refs.listGroup
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      selectItem(item) {
+        this.$emit('select', item)
+      }
     }
-  },
-  components: {
-    Scroll
   }
-};
 </script>
 <style lang="scss" scoped>
 @import "src/common/scss/variable.scss";
