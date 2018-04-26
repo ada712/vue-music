7d44c58c70f662395cd86ef8a98d929b004ad9dc
2014874201 <1152579039@qq.com>
2018-04-26 18:01:26 +0800

    11

---
+++ src/components/disc/disc.vue
@@ -0,0 +1,21 @@
+<template>
+  <transition name="slide">
+      <music-list></music-list>
+  </transition>
+</template>
+<script>
+import MusicList from 'components/musicList/musicList'
+export default {
+  components: {
+    MusicList
+  }
+}
+</script>
+<style lang="scss" scoped>
+.slide-enter-active, .slide-leave-active {  
+  transition: all 0.3s;
+}
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%,0,0)
+}
+</style>
